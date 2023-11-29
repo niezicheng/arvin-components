82424a8b9dc82d166870146f117f82d3a457c3fa
Arvinjun <niezicheng@shein.com>
2023-11-29 15:29:01 +0800

    build: dumirc base、publicPath config

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'dumi';
 
+const basePath = '/arvin-components/';
+
 export default defineConfig({
   outputPath: 'docs-dist',
   themeConfig: {
@@ -10,5 +12,7 @@ export default defineConfig({
   resolve: {
     // 配置入口文件路径，API 解析将从这里开始
     entryFile: './src/index.ts',
-  }
+  },
+  base: basePath,
+  publicPath: basePath,
 });
